8bed6f45f72cba1a88b00e49ab519b3fc9953748
marcos-martins <m.martins@outlook.com>
2018-11-14 23:00:01 +0000

    Create the controller calculator

---
+++ RestWebApi/RestWebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("{firstNUmber}/{secondNumber}")]
+        public ActionResult<int> Get(int firstNUmber, int secondNumber)
+        {
+            return firstNUmber+ secondNumber;
+        }
+    }
+}
